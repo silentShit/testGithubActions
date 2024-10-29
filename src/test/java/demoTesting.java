0433f09721f11227f434947c88c0e3afd9dfc587
silentShit <eddiutane@gmail.com>
2024-10-29 17:34:57 +0100

    Test 1

---
+++ src/test/java/demoTesting.java
@@ -0,0 +1,12 @@
+import org.junit.Assert;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+public class demoTesting {
+    @Test
+    public void aTest(){
+
+    Assertions.assertTrue(true);
+
+    }
+}
